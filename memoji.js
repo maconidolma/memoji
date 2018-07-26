d3cb2d20657c1f934a5fb2c1c47e66d24e742eca
maconidolma <linkova93@gmail.com>
2018-07-26 10:41:39 +0300

    implimented win condition with pop-window and timer

--- memoji.js
+++ memoji.js
@@ -1,6 +1,18 @@
 function initGame() {
 
-    //Constructor object DOM element - item
+    // State when 'nothing is rotated' or 'two red cards rotated'
+    var state1 = 1; 
+    // State when 'one card is rotated'
+    var state2 = 2;
+    // CurrentState
+    var currenState = state1;
+    // Last target from state1
+    var lastTarget = null;
+    var isFirstClick = true;
+
+    var setTimer = null;
+
+    // Constructor object DOM element - card
     function Card (domCard, content) {
         this._card = domCard;
         this.content = content;
@@ -36,20 +48,12 @@ function initGame() {
         this.removeRed = function () {
             this.front.classList.remove('red');
             this.isRed = false;
+        }
+        this.removeGreen = function() {
+            this.front.classList.remove('green');
+            this.isGreen = false;
         } 
     };
-
-    var cards = document.querySelectorAll('.card');
-    //array of items
-    var cardsArray = [];
-    var contents = ['🐓', '🐱', '🐿', '🐹', '🐨', '🐞', '🐓', '🐱', '🐿', '🐹', '🐨', '🐞'];
-    
-    //create by constructor item elemnts ans add them to array itemsArray
-    for (let i = 0; i < cards.length; i++) {
-        var count = parseInt(Math.random() * (contents.length - 1));
-        cardsArray.push(new Card(cards[i], contents[count]));
-        contents.splice(count, 1);
-    }  
 
     function findCardByDOMCard (DOMCard) {
         for (let i = 0; i < cardsArray.length; i++) {
@@ -60,22 +64,105 @@ function initGame() {
         throw "Bad DOWCard";
     }
 
+    var cards = document.querySelectorAll('.card');
+
+    var contents = ['🐓', '🐱', '🐿', '🐹', '🐨', '🐞', '🐓', '🐱', '🐿', '🐹', '🐨', '🐞'];
+    var cardsArray = getRandomNewCardsArray(contents);
+    
+    // Create cards with consrtuctor and store them to cadsArray
+    function getRandomNewCardsArray (contents) {
+        var cardsArray = [];
+        var contentsCopy = contents.slice();
+        for (let i = 0; i < cards.length; i++) {
+            var count = parseInt(Math.random() * (contentsCopy.length - 1));
+            cardsArray.push(new Card(cards[i], contentsCopy[count]));
+            contentsCopy.splice(count, 1);
+        } 
+        return cardsArray;
+    }
+    // Function refresh game
+    function resetGame() {
+        // If cards are rotated, red || green, remove this      
+        for (let i = 0; i < cardsArray.length; i++) {
+            if (cardsArray[i].isRotated) {
+                cardsArray[i].rotate();
+            }
+            if (cardsArray[i].isGreen) {
+                cardsArray[i].removeGreen();
+            }
+            if (cardsArray[i].isRed) {
+                cards[i].removeRed();
+            }
+        }
+        // Shuffle contents imoji
+        cardsArray = getRandomNewCardsArray(contents);
+        console.info(cardsArray);
+        // Reset the timer counter
+        var counterDOM = document.getElementById('count');
+        counterDOM.innerHTML = '01:00';
+        // Disappearance pop-up window
+        DisappearanceResult();
+
+        
+        state1 = 1; 
+        state2 = 2;
+        currenState = state1;
+        lastTarget = null;
+        isFirstClick = true;
+        setTimer = null;
+        counter = 60;
+    }
+
+    //  Appearance pop-up window
+    function appearanceResult(str1, str2) {
+        var popWindow = document.getElementById('invisible');
+        var resultMessage = document.getElementById('resultMessage');
+        var resultButton = document.getElementById('resultButton');
+        popWindow.classList.add('appearance');
+        resultMessage.innerHTML = str1;
+        resultButton.innerHTML = str2;
+    }
+    // Disappearance pop-up window
+    function DisappearanceResult() {
+        var popWindow = document.getElementById('invisible');
+        popWindow.classList.remove('appearance');
+    }
+
+    // Check is all cards are green
+    var isAllCardsGreen = function() {
+        for (let i = 0; i < cardsArray.length; i++) {
+            if (!cardsArray[i].isGreen) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    var counter = 60;
+    // Create timer finction
+    var timer = function() {
+        counter --;
+        if (counter >= 0) {
+            span = document.getElementById('count');
+            var time = new Date(counter * 1000);
+            var m = time.getMinutes();
+            var s = time.getSeconds();
+            if (s < 10) {
+                s = '0' + s;
+            }
+            span.innerHTML = m + ':' + s;
+        }
+        if (counter === 0) {
+            appearanceResult('Lose', 'Try again');
+            clearInterval(setTimer);
+        }
+    }
+
+    // Event handler for game
     var game = document.getElementById('game');
-
-    //state when nothing is rotated or two red cards rotated;
-    var state1 = 1; 
-
-    //state when one card is rotated;
-    var state2 = 2;
-
-    //currentState
-    var currenState = state1;
-
-    //last target in state 1
-    var lastTarget = null;
-
-    //event handler for game
     game.addEventListener('click', function(event) {
+
         if (! event.target.parentNode.classList.contains('card')) {
             return;
         }
@@ -86,7 +173,13 @@ function initGame() {
             card.rotate();
         }
 
-        //if current state is 'nothing is rotated' or 'two red cards rotated';
+        // Set timer for first click
+        if (isFirstClick) {
+            setTimer = setInterval(timer, 1000);
+            isFirstClick = false;
+        }
+
+        // If current state is 'nothing is rotated' or 'two red cards rotated'
         if (currenState == state1) {
             for (let i = 0; i < cardsArray.length; i++) {
                 if (cardsArray[i].isRed) {
@@ -98,7 +191,7 @@ function initGame() {
             currenState = state2;
         } 
 
-        //if current state is 'one card is rotated';
+        // If current state is 'one card is rotated'
         else if (currenState == state2) {
             if (lastTarget.content == card.content) {
                 lastTarget.setGreen();
@@ -110,8 +203,15 @@ function initGame() {
             currenState = state1;
             lastTarget = null;
         }
+
+        // Check win condition
+        if (isAllCardsGreen()) {
+            appearanceResult('Win', 'Play again');
+            clearInterval(setTimer);
+        }
     });
 
-    
-
+    // Event handler for refresh game
+    var button = document.getElementById('resultButton');
+    button.addEventListener('click', resetGame);
 }
